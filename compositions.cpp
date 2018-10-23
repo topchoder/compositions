864b9acfd567937d1b50ead852500b0973f985ba
Anurag Maurya <anuragmauryag@gmail.com>
2018-10-23 19:38:30 +0530

    Update compositions.cpp

--- compositions.cpp
+++ compositions.cpp
@@ -10,17 +10,17 @@ struct node
 node *nuget(int x)
 {
 	node *nu=new node;
-    nu->a=x;
-    nu->next=NULL;
-    return nu;
+   	nu->a=x;
+   	nu->next=NULL;
+    	return nu;
 }
 
 void coutin(node *root)
 {
 	while(root)
 	{
-		cout<<root->a<<" ";
-		root=root->next;
+	    cout<<root->a<<" ";
+	    root=root->next;
 	}
 	cout<<"\n";
 	return;
@@ -30,40 +30,41 @@ void permlist(int sum,int x,node **root,int n)
 {
 	if(!sum)//print the current composition
 	{
-		coutin(*root);
-		return;
+	     coutin(*root);
+	     return;
 	}
 	for(int i=1;i<=n;i++)
 	{
-		if(*root==NULL&&sum>=i)//if the current list is empty add the first
+	    if(*root==NULL&&sum>=i)//if the current list is empty add the first
+	    {
+		*root=nuget(i);
+                 permlist(sum-i,i,root,n);
+                 *root=NULL;//backtracking for other options
+	    }
+	    else if(*root&&sum>=i)//if the current list not empty
+	    {
+		node *tem=*root;
+		while(tem->next)//going to the last of current list
 		{
-			*root=nuget(i);
-       permlist(sum-i,i,root,n);
-      *root=NULL;//backtracking for other options
+		     tem=tem->next;
 		}
-		else if(*root&&sum>=i)//if the current list not empty
-		{
-			node *tem=*root;
-			while(tem->next)//going to the last of current list
-			{
-				tem=tem->next;
-			}
-			tem->next=nuget(i);
-      permlist(sum-i,i,root,n);
-      while(tem->next->next)//removing the last element to try other possibility
-      {
-         tem=tem->next;
-      }
-      node *tem1=tem->next;
-      tem->next=tem1->next;
-		}
-	}     	
+		tem->next=nuget(i);
+        	permlist(sum-i,i,root,n);
+      		while(tem->next->next)//removing the last element to try other possibility
+      		{
+         		tem=tem->next;
+      		}
+      		node *tem1=tem->next;
+      		tem->next=tem1->next;
+	    }
+	}  
+	return;
 }
 
 signed main()
 {
    	int sum=6;
-    node *root=NULL;//list to store the compositions
-    permlist(sum,1,&root,sum);
-	  return 0;
+    	node *root=NULL;//list to store the compositions
+    	permlist(sum,1,&root,sum);
+	return 0;
 }
